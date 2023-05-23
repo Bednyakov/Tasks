80bcc8715704bff972d59d717c33196204ccaf6a
Artem Bednyakov <artb@list.ru>
2023-05-23 16:34:46 +0300

    UPD Recursion block

--- Recursion (рекурсия)/05_list_of_lists_2/main.py
+++ Recursion (рекурсия)/05_list_of_lists_2/main.py
@@ -1,7 +1,7 @@
-nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
+nice_list = [1, 2, [3, [4]], [[[5, 6], 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-def openlist(obj, result=[]):
+def openlist(obj, result= []):
     for i in obj:
         if type(i) != list:
             result.append(i)
